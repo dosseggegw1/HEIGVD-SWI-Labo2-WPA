930e068247abdf505ec755393ad049bc14aca756
Dosseggegw1 <gwen.dos@bluewin.ch>
2021-04-07 18:57:05 +0200

    wpa_key_derivation ok

--- files/wpa_key_derivation.py
+++ files/wpa_key_derivation.py
@@ -13,8 +13,8 @@ __author__      = "Abraham Rubinstein et Yann Lederrey"
 __copyright__   = "Copyright 2017, HEIG-VD"
 __license__ 	= "GPL"
 __version__ 	= "1.0"
-__email__ 		= "abraham.rubinstein@heig-vd.ch"
-__status__ 		= "Prototype"
+__email__ 	= "abraham.rubinstein@heig-vd.ch"
+__status__ 	= "Prototype"
 
 from scapy.all import *
 from binascii import a2b_hex, b2a_hex
@@ -23,6 +23,7 @@ from pbkdf2 import *
 from numpy import array_split
 from numpy import array
 import hmac, hashlib
+from scapy.contrib.wpa_eapol import WPA_key
 
 def customPRF512(key,A,B):
     """
@@ -40,53 +41,67 @@ def customPRF512(key,A,B):
 # Read capture file -- it contains beacon, authentication, associacion, handshake and data
 wpa=rdpcap("wpa_handshake.cap")
 
-#print(wpa.summary())
-for trame in wpa:
-    #Get the Association request
-    if(trame.subtype == 0x0 and trame.type == 0x0):
-        APmac       =  a2b_hex((trame.addr1).replace(':',''))
-        Clientmac   =  a2b_hex((trame.addr2).replace(':',''))
-        ssid        = trame.info.decode("ascii")
-        print(APmac)
-        print(Clientmac)
-        print(ssid)
-        break
-#print(wpa[1].FCfield)
-#print(wpa[6].subtype)
-#print(wpa[6].type)
-i = 0
-print(wpa[6].info)
+#The network to attack
+ssid = "SWI"
 
+# Get the Association request who contains APmac address, Clientmac address and the ssid
+# We verify if the packet is from the network to attack
 for trame in wpa:
-    if (i == 1):
-        SNonce = trame.info[13:45] #baaaaaaaaaaaad
-        print(trame.show())
-        print(SNonce)
+    if trame.subtype == 0x0 and trame.type == 0x0 and trame.info.decode("ascii") == ssid:
+        APmac       =  a2b_hex(trame.addr1.replace(':',''))
+        Clientmac   =  a2b_hex(trame.addr2.replace(':',''))
         break
 
-    if(trame.subtype == 0x0 and trame.type == 0x2 ):
-        ANonce = trame.load[13:45]
-        i += 1
+compteur = 0
+
+#Get the ANonce based on the MAC address
+for trame in wpa:
+    if trame.subtype == 0x0 and trame.type == 0x2 and a2b_hex(trame.addr2.replace(':', '')) == APmac and a2b_hex(trame.addr1.replace(':', '')) == Clientmac:
+        ANonce = trame.getlayer(WPA_key).nonce
+        #print(b"nous A : "+ANonce)
+        #print(b"prof A : "+a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91"))
+        break
+    compteur += 1
+
+dejaPasse = False
+compteur2 = 0
+
+for trame in wpa:
+    #Get the SNonce based on the MAC address
+    if compteur2 > compteur and not(dejaPasse) and trame.subtype == 0x0 and trame.type == 0x0 and raw(trame)[18:24] == APmac and  raw(trame)[24:30] == Clientmac :
+        SNonce = raw(trame)[65:-72]
+        dejaPasse = True
+        compteur = compteur2
+        #print(b"nous S : "+SNonce)
+        #print(b"prof S : "+a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577"))
 
 
+    # Get the WPA key MIC
+    elif compteur2 > compteur and trame.subtype == 0x0 and trame.type == 0x0 and raw(trame)[18:24] == APmac and  raw(trame)[24:30] == Clientmac:
+        mic_to_test = raw(trame)[-18:-2].hex()
+        #print("nous mic : "+mic_to_test)
+        #print("prof mic : 36eef66540fa801ceee2fea9b7929b40")
+
+    compteur2 += 1
+
+#---------------------------------------------------------------
 # Important parameters for key derivation - most of them can be obtained from the pcap file
 passPhrase  = "actuelle"
 A           = "Pairwise key expansion" #this string is used in the pseudo-random function
+B           = min(APmac,Clientmac)+max(APmac,Clientmac)+min(ANonce,SNonce)+max(ANonce,SNonce) #used in pseudo-random function
+data        = a2b_hex("0103005f02030a0000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000") #cf "Quelques détails importants" dans la donnée
+
 #ssid        = "SWI"
 #APmac       = a2b_hex("cebcc8fdcab7")
 #Clientmac   = a2b_hex("0013efd015bd")
-
 # Authenticator and Supplicant Nonces
-ANonce      = a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
-SNonce      = a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
+#ANonce      = a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
+#SNonce      = a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
 
 # This is the MIC contained in the 4th frame of the 4-way handshake
 # When attacking WPA, we would compare it to our own MIC calculated using passphrases from a dictionary
-mic_to_test = "36eef66540fa801ceee2fea9b7929b40"
+# mic_to_test = "36eef66540fa801ceee2fea9b7929b40"
 
-B           = min(APmac,Clientmac)+max(APmac,Clientmac)+min(ANonce,SNonce)+max(ANonce,SNonce) #used in pseudo-random function
-
-data        = a2b_hex("0103005f02030a0000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000") #cf "Quelques détails importants" dans la donnée
 
 print ("\n\nValues used to derivate keys")
 print ("============================")
